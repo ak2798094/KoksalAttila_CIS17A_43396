1a22da74af10c7998086cb4ba62003cf8f23c05c
ak2798094 <akoksal@student.rccd.edu>
2021-03-05 22:12:03 -0800

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

---
+++ Homework/Review Homework 1/Gaddis_8_3_12_CelToFahr/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Attila Koksal
+ * Created on March 5, 2021, 9:04 PM
+ * Purpose:  CPP Template 
+ *           To be copied for each Assignment Problem
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    float Cel, Fahr;
+   
+    //Initialize Variables
+    cout<<"Input the Celsius (in degrees):"; 
+    cin>>Cel;
+    cout<<endl;
+     //Map Inputs to Outputs -> Process
+    Fahr=(9.0/5.0)*Cel+32;
+    
+    //Display Inputs/Outputs
+    cout<<"Fahrenheit (in degrees):"<<Fahr;
+    cout<<endl;
+    //Exit the Program - Cleanup
+    return 0;
+}
