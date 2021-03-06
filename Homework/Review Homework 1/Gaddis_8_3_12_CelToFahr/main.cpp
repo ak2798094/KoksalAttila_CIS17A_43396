1b3f190a7d0cb25e5d3aa13d4e79586d33ccf1c4
ak2798094 <akoksal@student.rccd.edu>
2021-03-05 22:18:14 -0800

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

--- Homework/Review Homework 1/Gaddis_8_3_12_CelToFahr/main.cpp
+++ Homework/Review Homework 1/Gaddis_8_3_12_CelToFahr/main.cpp
@@ -22,14 +22,14 @@ int main(int argc, char** argv) {
     //Initialize the Random Number Seed
     
     //Declare Variables
-    float Cel, Fahr;
+    float Cel, Fahr; //Celsius and Fahrenheit
    
     //Initialize Variables
     cout<<"Input the Celsius (in degrees):"; 
     cin>>Cel;
     cout<<endl;
      //Map Inputs to Outputs -> Process
-    Fahr=(9.0/5.0)*Cel+32;
+    Fahr=(9.0/5.0)*Cel+32; //Converts Celsius into Fahrenheit
     
     //Display Inputs/Outputs
     cout<<"Fahrenheit (in degrees):"<<Fahr;
