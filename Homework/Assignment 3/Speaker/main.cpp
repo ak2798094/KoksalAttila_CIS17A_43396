102c57252d10b74d8dc4f0e74e6c8286ab7326fa
ak2798094 <akoksal@student.rccd.edu>
2021-04-06 16:50:05 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

---
+++ Homework/Assignment 3/Speaker/main.cpp
@@ -0,0 +1,41 @@
+/* 
+ * File:   main.cpp
+ * Author: Attila Koksal
+ * Created on April 4, 2021, 12:10 PM
+ * Purpose:  CPP Template 
+ *           To be copied for each Assignment Problem
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+struct Speaker{
+    string name;
+    string phone;
+    string topic;
+    float fee;
+};
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map Inputs to Outputs -> Process
+    
+    //Display Inputs/Outputs
+    
+    //Exit the Program - Cleanup
+    return 0;
+}
