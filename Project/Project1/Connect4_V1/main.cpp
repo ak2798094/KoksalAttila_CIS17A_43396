2788971ba5846d18116facc71446586275352c6b
ak2798094 <akoksal@student.rccd.edu>
2021-04-17 20:39:27 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

---
+++ Project/Project1/Connect4_V1/main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   main.cpp
+ * Author: Attila Koksal
+ * Created on April 17, 2021, 8:30 PM
+ * Purpose:  CPP Template 
+ *           To be copied for each Assignment Problem
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+struct Space{
+    string type="";
+};
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map Inputs to Outputs -> Process
+    
+    //Display Inputs/Outputs
+    
+    //Exit the Program - Cleanup
+    return 0;
+}
