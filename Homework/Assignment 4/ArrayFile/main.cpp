12ad29e0d2457b7e58e69ea9e3e515b2c106aedd
ak2798094 <akoksal@student.rccd.edu>
2021-04-20 21:34:32 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

---
+++ Homework/Assignment 4/ArrayFile/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Attila Koksal
+ * Created on April 20, 2021, 9:05 PM
+ * Purpose:  CPP Template 
+ *           To be copied for each Assignment Problem
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+#include <fstream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+
+    //Initialize Variables
+    
+    //Map Inputs to Outputs -> Process
+    
+    //Display Inputs/Outputs
+    
+    //Exit the Program - Cleanup
+    return 0;
+}
