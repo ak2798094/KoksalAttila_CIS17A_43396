c034624751984f9209008ca16310245a92bb6a44
ak2798094 <akoksal@student.rccd.edu>
2021-04-21 02:06:05 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

--- Project/Project1/Connect4_V1/main.cpp
+++ Project/Project1/Connect4_V1/main.cpp
@@ -14,25 +14,99 @@ struct Space{ // Whether if a spot on the board is filled or not
     string value;
 };
 
-//User Libraries
 
-//Global Constants
-//Math, Science, Universal, Conversions, High Dimensioned Arrays
+struct Coordinate{
+	int x;
+  int y;
+};
 
-//Function Prototypes
 
-//Execution Begins Here
+const int ROW = 6;
+const int COLUMN = 7;
+
+
+
 int main(int argc, char** argv) {
-    //Initialize the Random Number Seed
+
+    Space board[ROW][COLUMN]; 
+		String player1 = "X";
+    String player2 = "O";
+
+
+	 
+
+		initialize(board); do this
+    printGame(); do this and this
+    X 	playerMove(board,"X"); //maybe
+  
+  
+  
+  
+		//headsOrTails();  // let user 1 chose heads or tails, then random generate result to chose order;
     
-    //Declare Variables
-    Space board[6][7];
-    //Initialize Variables
+    while(true){
     
-    //Map Inputs to Outputs -> Process
+      coord1= playerMove(board,player1);      
+
+     if (gameOver(board,coord1)){
+        cout<<"Game Over, player"<<player1<<"won";
+        break;
+      }
+	
+
+      coord2= playerMove(board,player2);
+       if (gameOver(board,coord2)){cout<<"Game Over, player "<<player2<<"won"; break;}
+	
+
+
+    }
     
-    //Display Inputs/Outputs
+
     
-    //Exit the Program - Cleanup
+    
+    
+    
     return 0;
 }
+
+ bool gameOver(Space board[Row][Column],Coordinate myCoord){
+
+
+
+  if (myCoordCausedWin){
+    return ;
+  }else{
+    return xxxx.
+  }
+
+}
+
+
+
+
+
+void initialize(Space board[ROW][COLUMN]){
+		for(int i=0;i<ROW;i++){
+			for(int j=0;j<COLUMN;j++){		
+      	board[i][j].value = " ";
+      }  
+    }
+}
+
+
+
+
+
+
+XXX playerMove(){
+
+cout<<enter which column to put;
+
+
+
+printGame();
+
+}
+
+
+
