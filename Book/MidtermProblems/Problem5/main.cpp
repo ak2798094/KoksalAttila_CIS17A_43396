1374ebf9e7fdebc803013d88ada35df53e85f0fc
ak2798094 <akoksal@student.rccd.edu>
2021-04-26 10:45:47 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

---
+++ Book/MidtermProblems/Problem5/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Attila Koksal
+ * Created on April 25, 2021, 9:30 AM
+ * Purpose:  Problem 5
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map Inputs to Outputs -> Process
+    
+    //Display Inputs/Outputs
+    
+    //Exit the Program - Cleanup
+    return 0;
+}
