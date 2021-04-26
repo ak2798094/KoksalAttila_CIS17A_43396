d8e1f438207a14dd3b781c1f2919cc9dcdbe0ccf
ak2798094 <akoksal@student.rccd.edu>
2021-04-26 10:58:14 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

---
+++ Book/MidtermProblems/Problem6/main.cpp
@@ -0,0 +1,37 @@
+/* 
+ * File:   main.cpp
+ * Author: Attila Koksal
+ * Created on April 25, 2021, 12:30 PM
+ * Purpose:  Problem 6
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map Inputs to Outputs -> Process
+    
+    //Display Inputs/Outputs
+    cout<<"a) The conversion of 2.875 into binary, octal, and hex"<<endl<<"Binary: "<<"10.111"<<endl<<"Octal: "<<"2.7"<<endl<<"Hex: "<<"2E"<<endl;
+    cout<<"The conversion of 0.1796875 into binary, octal, and hex"<<endl<<"Binary: "<<"0010111"<<endl<<"Octal: "<<"0.134"<<endl<<"Hex: "<<"0.2E"<<endl;
+    cout<<"b) The conversion of -2.875 into binary, octal, and hex"<<endl<<"Binary: "<<"-10.111"<<endl<<"Octal: "<<"-2.7"<<endl<<"Hex: "<<"-2E"<<endl;
+    cout<<"The conversion of -0.1796875 into binary, octal, and hex"<<endl<<"Binary: "<<"-0010111"<<endl<<"Octal: "<<"-0.134"<<endl<<"Hex: "<<"-0.2E"<<endl;
+    
+    //Exit the Program - Cleanup
+    return 0;
+}
