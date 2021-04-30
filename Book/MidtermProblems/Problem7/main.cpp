b78322d3cb432ee18d68ff8c82e0144e8a169f17
ak2798094 <akoksal@student.rccd.edu>
2021-04-29 17:49:46 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

--- Book/MidtermProblems/Problem7/main.cpp
+++ Book/MidtermProblems/Problem7/main.cpp
@@ -44,14 +44,46 @@ int main(int argc, char** argv) {
     cin>>number;
     Primes *p=factor(number);
     cout<<"Factors of "<<number<<": ";
-    print(p);
+    prntPrm(p);
     
     //Exit the Program - Cleanup
     return 0;
 }
 Primes *factor(int num){
-    
+    int start=2;
+    int facAry[10000];
+    while(num>1){
+        if(num%start==0){
+            facAry[start]++;
+            num=num/start;
+        }
+        else{
+            start++;
+        }
+    }
+    int i=0;
+    start=0;
+    while(i<10000){
+        if(facAry[i]!=0){
+            start+;
+        }
+        i++;
+    }
+    Primes *p=new Primes;
+    p->nPrimes=start;
+    p->prime=new Prime[start];
+    i=0;
+    start=0;
+    while(i<10000){
+        if(facAry[i]!=0){
+            p->prime[start].prime=i;
+            p->prime[start].power=facAry[i];
+            start++;
+        }
+        i++;
+    }
+    return p;
 }
-void print(Primes *prme){
+void prntPrm(Primes *prme){
     
 }
