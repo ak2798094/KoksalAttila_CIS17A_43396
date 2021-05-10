9a3537eef5ba9ddc78f3d2a48c70edf311fcd37f
ak2798094 <akoksal@student.rccd.edu>
2021-05-09 23:06:01 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

--- Project/Project1/Connect4_V1/main.cpp
+++ Project/Project1/Connect4_V1/main.cpp
@@ -45,17 +45,39 @@ void printGame(Space board[ROW][COLUMN]){
     }
 }
 
-void playerMove(Space board[ROW][COLUMN],string myPlayer){
+Coordinate playerMove(Space board[ROW][COLUMN],string myPlayer){
     int playRow = 0;
     int playCol=0;
-    cout<<"Player: "<<myPlayer<<". Enter which column to put: (0-"<<COLUMN-1<<")"<<endl;
-    cin>>playCol;
-    while(playCol>=COLUMN || playCol<0){
-        cout<<"Please enter again. Column Number needs to be less than "<<COLUMN<<" and greater than 0."<<endl;
+    Coordinate coord;
+    bool found=false;
+    
+    while(found==false){
+        
+        cout<<"Player: "<<myPlayer<<". Enter which column to put: (0-"<<COLUMN-1<<")"<<endl;
         cin>>playCol;
+        while(playCol>=COLUMN || playCol<0){
+           cout<<"Please enter again. Column Number needs to be less than "<<COLUMN<<" and greater than 0."<<endl;
+           cin>>playCol;
+        }
+    
+        for(int i=ROW-1;i>=0;i--){        
+            if(board[i][playCol].value=="_"){
+                found=true;
+                playRow=i;
+                break;
+            }
+        }
+        if (found==false){
+            printGame(board);
+            cout<<"Sorry, column "<<playCol<<" is full, please select another column."<<endl<<endl;
+        }
     }
     board[playRow][playCol].value=myPlayer;
-    printGame(board);
+    printGame(board);  
+    coord.x=playRow;
+    coord.y=playCol;
+    return coord;
+    
 }
 
 int main(int argc, char** argv) {
@@ -69,16 +91,23 @@ int main(int argc, char** argv) {
     initialize(board);
     printGame(board);
     
+    
+    
     while(true){
-    playerMove(board,player1);
-    playerMove(board,player2);
-
+        if(gameOver(board,playerMove(board,player1))){
+            break;
+        }
+        if(gameOver(board,playerMove(board,player2))){
+            break;
+        }
     }
     
     return 0;   
 }
 
  bool gameOver(Space board[ROW][COLUMN],Coordinate myCoord){
+    
+     
      return false;
 
 }
