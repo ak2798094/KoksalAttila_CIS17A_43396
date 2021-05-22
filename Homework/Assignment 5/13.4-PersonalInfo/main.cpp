38a764f50df7069e41a213304b9e13cc6bd101de
ak2798094 <akoksal@student.rccd.edu>
2021-05-21 23:32:24 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

---
+++ Homework/Assignment 5/13.4-PersonalInfo/main.cpp
@@ -0,0 +1,67 @@
+/* 
+ * File:   main.cpp
+ * Author: Attila Koksal
+ * Created on May 20, 2021, 2:35 PM
+ * Purpose:  CPP Template 
+ *           To be copied for each Assignment Problem
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+class PersonalInfo{
+private:
+    string name;
+    string address;
+    int age;
+    int phone;
+public:
+    void setName(string nme){
+        name=nme;
+    }
+    void setAddress(string addrs){
+        address=addrs;
+    }
+    void setAge(int a){
+        age=a;
+    }
+    void setPhone(int phne){
+        phone=phne;
+    }
+    string getName(){
+        return name;
+    }
+    string getAddress(){
+        return address;
+    }
+    int getAge(){
+        return age;
+    }
+    int getPhone(){
+        return phone;
+    }
+};
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map Inputs to Outputs -> Process
+    
+    //Display Inputs/Outputs
+    
+    //Exit the Program - Cleanup
+    return 0;
+}
