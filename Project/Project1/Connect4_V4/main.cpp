a6e9683d832154efb0d581a491d42223041f540d
ak2798094 <akoksal@student.rccd.edu>
2021-05-22 23:53:34 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

--- Project/Project1/Connect4_V4/main.cpp
+++ Project/Project1/Connect4_V4/main.cpp
@@ -7,9 +7,9 @@
  */
 
 //System Libraries
-#include <iostream>  //I/O Library
+#include <iostream>  // I/O Library
 #include <fstream>   // F Stream Library
-#include <string>
+#include <string>    // String Library
 using namespace std;
 
 enum Piece{//Piece represents all possible values of a spot on a board can be, in traditional board, its spot can be either red or yellow, or empty. Here we use X and O to represent yellow and empty
@@ -34,8 +34,8 @@ void initialize(Space[ROW][COLUMN]); // Initializing the game board to where all
 void printGame(Space[ROW][COLUMN]); // Prints the game board onto the screen
 Coordinate playerMove(Space[ROW][COLUMN],Piece); // Plays and prints to a specific spot on the board according to the player's input
 bool gameOver(Space[ROW][COLUMN],Coordinate); // Checks if the game is over by checking win conditions (Horizontal, Vertical, Diagonal Slash and Backslash, and Draw)
-void saveGame(Space[ROW][COLUMN]);
-bool loadGame(Space[ROW][COLUMN],Piece);
+void saveGame(Space[ROW][COLUMN]); // Saves the game whenever the user wants to
+bool loadGame(Space[ROW][COLUMN],Piece); // Loads the saved game when the user decides to resume the unfinished game
 
 
 int main(int argc, char** argv) {
@@ -56,24 +56,24 @@ int main(int argc, char** argv) {
     printGame(board); // Prints the game board onto the screen
     
     while(true){ // Using a while loop to check if a player's move resulted in victory
-        turnCnt++;
-        coord1=playerMove(board,player1);
-        if (coord1.x!=-1){
+        turnCnt++; // Increments turn count whenever a player moves
+        coord1=playerMove(board,player1); // Assigns coord1 variable to player1's move
+        if(coord1.x!=-1){ // If player1 loaded the game and if it's player2's turn, then skip win check
         if(gameOver(board,coord1)){ // Checks if player1's move on the game board resulted in victory
             cout<<" The game ended after "<<turnCnt<<" turns.";
             break; // If victory, the program exits out of the game
         }
         }
         
-        coord2=playerMove(board,player2);
-        if (coord2.x!=-1){
+        coord2=playerMove(board,player2); // Assigns coord2 variable to player2's move
+        if(coord2.x!=-1){ // If player2 loaded the game and if it's player1's turn, then skip win check
         if(gameOver(board,coord2)){ // Checks if player2's move on the game board resulted in victory
-            cout<<"The game ended after "<<turnCnt<<" turns.";
+            cout<<"The game ended after "<<turnCnt<<" turns."; // Prints a message
             break; // If victory, the program exits out of the game
         }
         }
     }
-    cout<<endl<<"Thank you for playing the game."<<endl;
+    cout<<endl<<"Thank you for playing the game."<<endl; // Prints a message
     
     return 0;   
 }
@@ -127,17 +127,16 @@ Coordinate playerMove(Space board[ROW][COLUMN],Piece myPlayer){
     
     while(found==false){ // while user inputs is not valid, we keep asking user to try again
         
-        cin>>input;
+        cin>>input; // gets the input from the user
         
-        if (input[0]=='x'||input[0]=='X'){
-            saveGame(board);
+        if(input[0]=='x'||input[0]=='X'){//if user input x, or X, save game
+            saveGame(board); // saves the game board
             cout<<"Player: "<<playerLetter<<". Enter which column to put: (0-"<<COLUMN-1<<"), or 'X'to save and quit,'L' to load game"<<endl; // ask for input
-            continue;
-        }else if (input[0]=='L'||input[0]=='l'){
-            
-            if (!loadGame(board,myPlayer)){Coordinate returnCord;returnCord.x=-1;returnCord.y=-1;return returnCord;}
+            continue;// if it is not valid, go back to ask input again
+        }else if(input[0]=='L'||input[0]=='l'){//if user input l, or L, load game
+            if(!loadGame(board,myPlayer)){Coordinate returnCord;returnCord.x=-1;returnCord.y=-1;return returnCord;}//if when loaded game, it is next player's turn instead, end function and return coord with -1 for x and y
             cout<<"Player: "<<playerLetter<<". Enter which column to put: (0-"<<COLUMN-1<<"), or 'X'to save and quit,'L' to load game"<<endl; // ask for input
-            continue;
+            continue;// if it is not valid, go back to ask input again
         }
         
         // Checks if input is letter, word, or number
@@ -173,51 +172,51 @@ Coordinate playerMove(Space board[ROW][COLUMN],Piece myPlayer){
     coord.y=playCol; //prep to return the y info for later result check
     return coord;   //Return the info for later result check
 }
-void saveGame(Space board[ROW][COLUMN]){
-    fstream file;
-    file.open("save.dat",ios::out|ios::binary);
-    for(int i=0;i<ROW;i++){
-        for(int j=0;j<COLUMN;j++){
-            char writeTarget;
-            if (board[i][j].value==X){writeTarget='X';}
-            else if (board[i][j].value==O){writeTarget='O';}
-            else if (board[i][j].value==Empty){writeTarget='_';}
+void saveGame(Space board[ROW][COLUMN]){// save the current board to be loaded at another time
+    fstream file; //create file instance
+    file.open("save.dat",ios::out|ios::binary); // open save.dat file
+    for(int i=0;i<ROW;i++){ //for all rows
+        for(int j=0;j<COLUMN;j++){ // for all columns
+            char writeTarget; //create char to be written to
+            if (board[i][j].value==X){writeTarget='X';}// if player X is at the location, write X in file
+            else if (board[i][j].value==O){writeTarget='O';} // if player O is at the location, write O in file
+            else if (board[i][j].value==Empty){writeTarget='_';} // if no player is at the location, write _ in file
           
-            file.write(&writeTarget,(sizeof(char)));
+            file.write(&writeTarget,(sizeof(char)));//write to document
         }
     }
-    file.close();
-    cout<<"Game Saved"<<endl;
+    file.close();//close file
+    cout<<"Game Saved"<<endl;//show message to confirm saving
 }
-bool loadGame(Space board[ROW][COLUMN],Piece myPlayer){
-    fstream file;
-    file.open("save.dat",ios::in|ios::binary);
-    int playerXCount = 0;
-    int playerOCount = 0;
-    bool result =true;
-    for(int i=0;i<ROW;i++){
-        for(int j=0;j<COLUMN;j++){
-            char ch;
-            file.read(&ch,(sizeof(ch)));
-           if (ch=='X'){board[i][j].value=X;playerXCount++;}
-           else if (ch=='O'){board[i][j].value=O;playerOCount++;}
-           else if (ch=='_'){board[i][j].value=Empty;}
+bool loadGame(Space board[ROW][COLUMN],Piece myPlayer){//when user enter "L" or "l". update board to reflect saved game and then the next player would play
+    fstream file;//create file
+    file.open("save.dat",ios::in|ios::binary);//open file to read
+    int playerXCount = 0;//counter check how many x pieces are there,to see whos turn is next
+    int playerOCount = 0;//counter check how many o pieces are there,to see whos turn is next
+    bool result =true;// if it is not "myPlayer"'s turn next, return false
+    for(int i=0;i<ROW;i++){//for each row
+        for(int j=0;j<COLUMN;j++){//for each column
+            char ch;//character to read file with
+            file.read(&ch,(sizeof(ch)));//read char from file
+           if (ch=='X'){board[i][j].value=X;playerXCount++;}//if character read is X, put that value on board then add to counter 
+           else if (ch=='O'){board[i][j].value=O;playerOCount++;}//if character read is O, put that value on board then add to counter 
+           else if (ch=='_'){board[i][j].value=Empty;}//if character read is _, put that value on board then add to counter 
            // cout<<"char:"<<ch;
             
             
             
         }
-        if (myPlayer==X && (playerXCount>playerOCount)){
-            result = false;
+        if (myPlayer==X && (playerXCount>playerOCount)){// if it is player X's turn but loaded game indicates that O should play next
+            result = false;//return false
         }
-        else if (myPlayer==O && (playerXCount<=playerOCount)){
-            result = false;
+        else if (myPlayer==O && (playerXCount<=playerOCount)){// if it is player O's turn but loaded game indicates that X should play next
+            result = false;//return false
         }
     }
-         printGame(board);
+         printGame(board);//show board
             //cout<<"Loaded Game from Previous Save"<<endl;
-    file.close();
-    return result;
+    file.close();//close file
+    return result;//return result
 }
 
 
