f80e8c11f03c7373eb8dc04bc73d6ae6702e0db8
ak2798094 <akoksal@student.rccd.edu>
2021-05-23 16:33:07 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

--- Homework/Assignment 5/14.4-NumDays/main.cpp
+++ Homework/Assignment 5/14.4-NumDays/main.cpp
@@ -12,26 +12,57 @@ using namespace std;
 
 class NumDays{
 private:
-    int hours;
+    float hours;
     float days;
 public:
-    NumDays(){
-        hours=0;
-        days=hours/8.0;
+    NumDays(float h=0){
+        hours=h;
+        days=h/(8.00);
     }
-    NumDays(int a){
-        hours=a;
-        days=hours/8.0;
+    float getHours(){
+        return hours;
     }
-    void setHours(int);
-    int getHour()const{return hours;}
-    float getDay()const{return days;}
-    int operator+(NumDays &);
-    int operator-(NumDays &);
-    int operator++(int);
-    int operator++();
-    int operator--(int);
-    int operator--();
+    float getDays(){
+        return days;
+    }
+    void setHours(float h){
+        hours=h;
+        days=h/(8.00);
+    }
+    void setDays(float d){
+        days=d;
+        hours=d*(8.00);
+    }
+    float operator+(const NumDays &right){
+        return hours+right.hours;
+    }
+    float operator-(const NumDays &right){
+        if(hours<right.hours){
+            cout<<"Error! Can't solve. Exiting";
+            exit(0);
+        }
+        return hours-right.hours;
+    }
+    NumDays operator++(){
+        ++hours;
+        days=hours/(8.00);
+        return *this;
+    }
+    NumDays operator++(int){
+        hours++;
+        days=hours/(8.00);
+        return *this;
+    }
+    NumDays operator--(){
+        --hours;
+        days=hours/(8.00);
+        return *this;
+    }
+    NumDays operator--(int){
+        hours--;
+        days=hours/(8.00);
+        return *this;
+    }
 };
 
 //User Libraries
@@ -52,42 +83,36 @@ int main(int argc, char** argv) {
     //Map Inputs to Outputs -> Process
     
     //Display Inputs/Outputs
+    //create first test
+    cout<<"Creating test with 12 hours..."<<endl;
+    NumDays test1(12);
+    cout<<test1.getHours()<<" hours = "<<test1.getDays()<<" days."<<endl;
+
+    //create second test
+    cout<<endl<<"Creating test with 18 hours..."<<endl;
+    NumDays test2(18);
+    cout<<test2.getHours()<<" hours = "<<test2.getDays()<<" days."<<endl;
+
+    //test overloaded + operator
+    cout<<endl<<"Adding hours... "<<test1+test2<<" hours."<<endl;
+
+    //test overloaded - operator
+    cout<<endl<<"Subtracting hours... "<<test2-test1<<" hours."<<endl<<endl;
+
+    //test overloaded ++ operators
+    cout<<"Pre- and post-incrementing first test..."<<endl;
+    ++test1;
+    cout<<test1.getHours()<<" hours = "<<test1.getDays()<<" days."<<endl;
+    test1++;
+    cout<<test1.getHours()<<" hours = "<<test1.getDays()<<" days."<<endl;
+
+    //test overloaded -- operators
+    cout<<endl<<"Pre- and post-decrementing second test..."<<endl;
+    --test2;
+    cout<<test2.getHours()<<" hours = "<<test2.getDays()<<" days."<<endl;
+    test2--;
+    cout<<test2.getHours()<<" hours = "<<test2.getDays()<<" days."<<endl;
     
     //Exit the Program - Cleanup
     return 0;
-}
-void NumDays::setHours(int a){
-    hours=a;
-}
-int NumDays::operator+(NumDays &right){
-    int totalHours;
-    totalHours=hours+right.hours;
-    return totalHours;
-}
-int NumDays::operator-(NumDays &right){
-    int totalHours;
-    totalHours=hours-right.hours;
-    return totalHours;
-}
-int NumDays::operator++(int){
-    int num=hours;
-    hours++;
-    days=hours/8.0;
-    return num;
-}
-int NumDays::operator++(){
-    hours++;
-    days=hours/8.0;
-    return hours;
-}
-int NumDays::operator--(int){
-    int num=hours;
-    hours--;
-    days=hours/8.0;
-    return num;
-}
-int NumDays::operator--(){
-    hours--;
-    days=hours/8.0;
-    return hours;
 }
