b1d43938b98e340da9fbf62087559bac3d2c247d
ak2798094 <akoksal@student.rccd.edu>
2021-05-23 17:27:16 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

--- Homework/Assignment 5/14.10-CorporateSales/main.cpp
+++ Homework/Assignment 5/14.10-CorporateSales/main.cpp
@@ -8,7 +8,79 @@
 
 //System Libraries
 #include <iostream>  //I/O Library
+#include "NumDays.h" // NumDays class file
 using namespace std;
+
+class NumDays;
+
+class TimeOff{
+private:
+    string name;
+    string IDnum;
+    NumDays maxSickDays,sickTaken,maxVacation,vacTaken,maxUnpaid,unpaidTaken;
+
+public:
+    TimeOff(string n="", string id=""){
+        name=n;
+        IDnum=id;
+    }
+    string getName()const{
+        return name;
+    }
+    string getID()const{
+        return IDnum;
+    }
+    float getMaxSickDays(){
+        return maxSickDays.getDays();
+    }
+    float getSickTaken(){
+        return sickTaken.getDays();
+    }
+    float getMaxVacation(){
+        return maxVacation.getDays();
+    }
+    float getVacTaken(){
+        return vacTaken.getDays();
+    }
+    float getMaxUnpaid(){
+        return maxUnpaid.getDays();
+    }
+    float getUnpaidTaken(){
+        return unpaidTaken.getDays();
+    }
+
+    //-------------------------------------------------
+
+    void setName(string n){
+        name=n;
+    }
+    void setID(string id){
+        IDnum=id;
+    }
+    void setMaxSickDays(float h){
+        maxSickDays.setHours(h);
+    }
+    void setSickTaken(float h){
+        sickTaken.setHours(h);
+    }
+    void setMaxVacation(float h){
+        if(h>240){
+            maxVacation.setHours(240);
+        }
+        else{
+            maxVacation.setHours(h);
+        }
+    }
+    void setVacTaken(float h){
+        vacTaken.setHours(h);
+    }
+    void setMaxUnpaid(float h){
+        maxUnpaid.setHours(h);
+    }
+    void setUnpaidTaken(float h){
+        unpaidTaken.setHours(h);
+    }
+};
 
 //User Libraries
 
