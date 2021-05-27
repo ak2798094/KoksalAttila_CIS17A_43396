b2478287f5f6d7e367a74a7d8ddffb4a76295a38
ak2798094 <akoksal@student.rccd.edu>
2021-05-26 22:28:42 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

--- Project/Project2/Chess_V1/main.cpp
+++ Project/Project2/Chess_V1/main.cpp
@@ -16,23 +16,57 @@ enum Piece{//Piece represents all possible values of a spot on a board can be, i
     Bishop,
     Knight,
     Queen,
-    King
+    King,
+    Empty
 };
 
-struct Space{ // Whether if a spot on the board is filled or not
+enum Color{
+    WHITE,
+    BLACK,
+    NONE
+};
+
+class Space{ // Whether if a spot on the board is filled or not
     Piece value;
+    Color color;
+    
+public:
+    Color getColor();
+    Piece getValue();
+    void setValue(Piece);
+    
 };
 
 struct Coordinate{
     int x;
     int y;
 };
+
+class Board{
+    Space board[ROW][COLUMN];
+    Color turn=WHITE;
+    
+public:
+    void initializeBoard();
+    Piece getPiece(Coordinate);
+    void movePiece(Coordinate,Coordinate);
+    void printBoard();
+    void setPiece();
+};
+
 const int ROW = 8;
 const int COLUMN = 8;
 
 int main(int argc, char** argv) {
     
-    Space board[ROW][COLUMN];
+    Board board;
     
+    board.initializeBoard();
+    
+    
+    
+}
+
+void Board::initializeBoard(){
     
 }
