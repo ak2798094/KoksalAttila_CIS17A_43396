6bcebcf111aabddf5f4f9a09da3df59be511d37e
ak2798094 <akoksal@student.rccd.edu>
2021-05-27 20:08:23 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

--- Homework/Assignment 6/16.1-DateExceptions/main.cpp
+++ Homework/Assignment 6/16.1-DateExceptions/main.cpp
@@ -1,25 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.cpp
- * Author: attil
- *
- * Created on May 27, 2021, 8:06 PM
+ * Author: Attila Koksal
+ * Created on May 27, 2021, 10:45 AM
+ * Purpose:  CPP Template 
+ *           To be copied for each Assignment Problem
  */
 
-#include <cstdlib>
-
+//System Libraries
+#include <iostream>  //I/O Library
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+//User Libraries
 
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map Inputs to Outputs -> Process
+    
+    //Display Inputs/Outputs
+    
+    //Exit the Program - Cleanup
     return 0;
 }
-
