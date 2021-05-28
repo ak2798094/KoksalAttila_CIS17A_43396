e13d299bd9eb006416f3d31705e5fbfa1f3b7a2a
ak2798094 <akoksal@student.rccd.edu>
2021-05-28 12:53:46 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

--- Homework/Assignment 6/16.3-MinimumMaximumTemplates/main.cpp
+++ Homework/Assignment 6/16.3-MinimumMaximumTemplates/main.cpp
@@ -10,6 +10,26 @@
 #include <iostream>  //I/O Library
 using namespace std;
 
+template <class T>
+T Min(T num1,T num2){
+    if(num1<num2){
+        return num1;
+    }
+    else{
+        return num2;
+    }
+}
+
+template <class T>
+T Max(T num1,T num2){
+    if(num1>num2){
+        return num1;
+    }
+    else{
+        return num2;
+    }
+}
+
 //User Libraries
 
 //Global Constants
@@ -22,12 +42,29 @@ int main(int argc, char** argv) {
     //Initialize the Random Number Seed
     
     //Declare Variables
+    int int1,int2;
+    double doub1,doub2;
+    string char1,char2;
     
     //Initialize Variables
     
     //Map Inputs to Outputs -> Process
     
     //Display Inputs/Outputs
+    cout<<"Enter two integer values with an enter after the first:"<<endl;
+    cin>>int1>>int2;
+    cout<<"Lesser: "<<Min(int1,int2)<<endl;
+    cout<<"Greater: "<<Max(int1,int2)<<endl;
+    
+    cout<<"Enter two float values with an enter after the first:"<<endl;
+    cin>>doub1>>doub2;
+    cout<<"Lesser: "<<Min(doub1,doub2)<<endl;
+    cout<<"Greater: "<<Max(doub1,doub2)<<endl;
+    
+    cout<<"Enter two strings values with an enter after the first:"<<endl;
+    cin>>char1>>char2; 
+    cout<<"Lesser: "<<Min(char1,char2)<<endl;
+    cout<<"Greater: "<<Max(char1,char2)<<endl;
     
     //Exit the Program - Cleanup
     return 0;
