276cbc707d3be59f46b8c581e382f46a75256b41
ak2798094 <akoksal@student.rccd.edu>
2021-06-03 15:48:37 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

--- Book/Final/FinalProblem2/main.cpp
+++ Book/Final/FinalProblem2/main.cpp
@@ -10,6 +10,17 @@
 #include <iostream>  //I/O Library
 using namespace std;
 
+template<class T>
+class Prob2Sort{
+    private:
+        int *index;                                 //Index that is utilized in the sort
+    public:
+        Prob2Sort(){index=NULL;};                   //Constructor
+        ~Prob2Sort(){delete []index;};              //Destructor
+        T * sortArray(const T*,int,bool);           //Sorts a single column array
+        T * sortArray(const T*,int,int,int,bool);   //Sorts a 2 dimensional array represented as a 1 dim array
+}; 
+
 //User Libraries
 
 //Global Constants
@@ -22,13 +33,34 @@ int main(int argc, char** argv) {
     //Initialize the Random Number Seed
     
     //Declare Variables
+    Prob2Sort<char> rc;
+    bool ascending=true;
+    ifstream infile;
+    infile.open("Problem2.txt",ios::in);
+    char *ch2=new char[10*16];
+    char *ch2p=ch2;
     
     //Initialize Variables
     
     //Map Inputs to Outputs -> Process
     
     //Display Inputs/Outputs
+    cout<<"The start of Problem 2, the sorting problem"<<endl;
+    while(infile.get(*ch2)){cout<<*ch2;ch2++;}
+    infile.close();
+    cout<<endl;
+    cout<<"Sorting on which column"<<endl;
+    int column;
+    cin>>column;
+    char *zc=rc.sortArray(ch2p,10,16,column,ascending);
+    for(int i=0;i<10;i++){
+        for(int j=0;j<16;j++){
+            cout<<zc[i*16+j];
+        }
+    }
     
     //Exit the Program - Cleanup
+    delete []zc;
+    cout<<endl;
     return 0;
 }
