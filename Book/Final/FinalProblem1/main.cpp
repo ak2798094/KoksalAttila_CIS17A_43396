3080a292bd95fccc05550b5c3d2ec7adc3faaad7
ak2798094 <akoksal@student.rccd.edu>
2021-06-03 15:36:55 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

---
+++ Book/Final/FinalProblem1/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Attila Koksal
+ * Created on June 3, 2021, 5:30 PM
+ * Purpose:  CPP Template 
+ *           Problem 1 of Final
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map Inputs to Outputs -> Process
+    
+    //Display Inputs/Outputs
+    
+    //Exit the Program - Cleanup
+    return 0;
+}
