5735f5136c87cb3b76af2dc9e7c9533a0c122b4c
ak2798094 <akoksal@student.rccd.edu>
2021-06-06 13:12:09 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

---
+++ Book/Final/FinalProblem6/main.cpp
@@ -0,0 +1,49 @@
+/* 
+ * File:   main.cpp
+ * Author: Attila Koksal
+ * Created on June 8, 2021, 6:30 PM
+ * Purpose:  CPP Template 
+ *           Problem 6 of Final
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map Inputs to Outputs -> Process
+    
+    //Display Inputs/Outputs
+    
+    /* Given the following base 10 decimals
+    a)3.75
+    b)0.7
+    c)89.9
+    1) Convert to binary, octal and hex, then 
+    2) Convert to NASA Hex float with first 24 bits representing the
+    signed fraction and the last 8 bits representing the
+    signed exponent. Scaled as 0.FRACTION x 2^EXPONENT
+    3) convert a) to scaled integer binary 1 unsinged byte maxium bits,
+       convert b) to scaled integer binary 2 unsinged byte maxium bits,
+       convert c) to scaled integer binary 3 unsinged byte maxium bits,
+    4) multiply 3) by 1 byte value 7 then shift back to integer and output the result
+    5) Then convert using IEEE 754 format
+    */
+    
+    //Exit the Program - Cleanup
+    return 0;
+}
