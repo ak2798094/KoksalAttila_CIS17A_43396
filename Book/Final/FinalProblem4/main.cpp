198212ea44c0a3ade3a10b09499ef4f2dc4126d2
ak2798094 <akoksal@student.rccd.edu>
2021-06-10 21:16:12 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

--- Book/Final/FinalProblem4/main.cpp
+++ Book/Final/FinalProblem4/main.cpp
@@ -8,18 +8,49 @@
 
 //System Libraries
 #include <iostream>  //I/O Library
+#include <cmath>
 using namespace std;
 
 class SavingsAccount{
     public:
-        SavingsAccount(float);//Constructor
-        void Transaction(float);//Procedure
-        float Total(float=0,int=0);//Savings Procedure
-        float TotalRecursive(float=0,int=0);
+        SavingsAccount(float balance){//Constructor
+            Balance=balance;
+            FreqWithDraw=0;
+            FreqDeposit=0;
+        }
+        void Transaction(float transAmount){//Procedure
+            if(transAmount>0){
+                Balance=Deposit(transAmount);
+            }
+            else if(transAmount<0){
+                Balance=Withdraw(-transAmount);
+            }
+        }
+        float Total(float savings=0,int time=0){//Savings Procedure
+            float newBalance=Balance;
+            for(int i=0;i<time;i++){
+                newBalance=newBalance*(savings+1.0);
+            }
+            return newBalance;
+        }
+        float TotalRecursive(float savings=0,int time=0){
+            
+        }
         void toString();//Output Properties
     private:
-        float Withdraw(float);//Utility Procedure
-        float Deposit(float);//Utility Procedure
+        float Withdraw(float transA){//Utility Procedure
+            FreqWithDraw++;
+            float newBalance=Balance-transA;
+            if(newBalance<0){
+                cout<<"You are trying to make a withdrawal that exceeds your balance."<<endl;
+                newBalance=Balance;
+            }
+            return newBalance;
+        }
+        float Deposit(float transA){//Utility Procedure
+            FreqDeposit++;
+            return Balance+transA;
+        }
         float Balance;//Property
         int FreqWithDraw;//Property
         int FreqDeposit;//Property
@@ -49,11 +80,8 @@ int main(int argc, char** argv) {
         mine.Transaction((float)(rand()%500)*(rand()%3-1));
     }
     mine.toString();
-    cout<<"Balance after 7 years given 10% interest = "
-            <<mine.Total((float)(0.10),7)<<endl;
-    cout<<"Balance after 7 years given 10% interest = "
-            <<mine.TotalRecursive((float)(0.10),7)
-            <<" Recursive Calculation "<<endl;
+    cout<<"Balance after 7 years given 10% interest = "<<mine.Total((float)(0.10),7)<<endl;
+    cout<<"Balance after 7 years given 10% interest = "<<mine.TotalRecursive((float)(0.10),7)<<" Recursive Calculation "<<endl;
     
     //Exit the Program - Cleanup
     return 0;
