f73336559e5c6a979905b01e28f649e3e9cf6382
ak2798094 <akoksal@student.rccd.edu>
2021-06-11 23:43:58 -0700

    Signed-off-by: ak2798094 <akoksal@student.rccd.edu>

--- Book/Final/FinalMenu/main.cpp
+++ Book/Final/FinalMenu/main.cpp
@@ -547,5 +547,19 @@ void prblm5(){
 // Problem 6
 void prblm6(){
     cout<<"Problem 6"<<endl;
-    
+    cout<<"1) a) 3.75 in Binary: 11.11,Octal: 3.6,Hex: 3C"<<endl;
+    cout<<"1) b) 0.7 in Binary: 10110011,Octal: 54631463,Hex: B3"<<endl;
+    cout<<"1) c) 89.9 in Binary: 1011001.1110011,Octal: 131.71463,Hex: 59.E6"<<endl;
+    cout<<"2) a) 3.75 in NASA: 400E000000000000"<<endl;
+    cout<<"2) b) 0.7 in NASA: 3FE6666666666666"<<endl;
+    cout<<"2) c) 89.9 in NASA: 405679999999999A"<<endl;
+    cout<<"3) a) 3.75 in scaled binary 1: 11.1"<<endl;
+    cout<<"3) b) 0.7 in scaled binary 2: 101100"<<endl;
+    cout<<"3) c) 89.9 in scaled binary 3: 1011001.11"<<endl;
+    cout<<"4) a) 3.75 in scaled binary 1(multiplied by 1 byte): 88.8"<<endl;
+    cout<<"4) b) 0.7 in scaled binary 2(multiplied by 1 byte): 808800"<<endl;
+    cout<<"4) c) 89.9 in scaled binary 3(multiplied by 1 byte): 8088008.88"<<endl;
+    cout<<"5) a) 3.75 in IEE 754: 1.875"<<endl;
+    cout<<"5) b) 0.7 in IEE 754: 1.4"<<endl;
+    cout<<"5) c) 89.9 in IEE 754: 1.4046875"<<endl;
 }
--- Book/Final/FinalProblem6/main.cpp
+++ Book/Final/FinalProblem6/main.cpp
@@ -29,24 +29,21 @@ int main(int argc, char** argv) {
     
     //Display Inputs/Outputs
     
-    /* Given the following base 10 decimals
-    a)3.75
-    b)0.7
-    c)89.9
-    1) Convert to binary, octal and hex, then 
-    2) Convert to NASA Hex float with first 24 bits representing the
-    signed fraction and the last 8 bits representing the
-    signed exponent. Scaled as 0.FRACTION x 2^EXPONENT
-    3) convert a) to scaled integer binary 1 unsinged byte maxium bits,
-       convert b) to scaled integer binary 2 unsinged byte maxium bits,
-       convert c) to scaled integer binary 3 unsinged byte maxium bits,
-    4) multiply 3) by 1 byte value 7 then shift back to integer and output the result
-    5) Then convert using IEEE 754 format
-    */
-    cout<<"1) a) 3.75 in Binary: ,Octal: ,Hex: "<<endl;
-    cout<<"1) b) 0.7 in Binary: ,Octal: ,Hex: "<<endl;
-    cout<<"1) c) 89.9 in Binary: ,Octal: ,Hex: "<<endl;
-    
+    cout<<"1) a) 3.75 in Binary: 11.11,Octal: 3.6,Hex: 3C"<<endl;
+    cout<<"1) b) 0.7 in Binary: 10110011,Octal: 54631463,Hex: B3"<<endl;
+    cout<<"1) c) 89.9 in Binary: 1011001.1110011,Octal: 131.71463,Hex: 59.E6"<<endl;
+    cout<<"2) a) 3.75 in NASA: 400E000000000000"<<endl;
+    cout<<"2) b) 0.7 in NASA: 3FE6666666666666"<<endl;
+    cout<<"2) c) 89.9 in NASA: 405679999999999A"<<endl;
+    cout<<"3) a) 3.75 in scaled binary 1: 11.1"<<endl;
+    cout<<"3) b) 0.7 in scaled binary 2: 101100"<<endl;
+    cout<<"3) c) 89.9 in scaled binary 3: 1011001.11"<<endl;
+    cout<<"4) a) 3.75 in scaled binary 1(multiplied by 1 byte): 88.8"<<endl;
+    cout<<"4) b) 0.7 in scaled binary 2(multiplied by 1 byte): 808800"<<endl;
+    cout<<"4) c) 89.9 in scaled binary 3(multiplied by 1 byte): 8088008.88"<<endl;
+    cout<<"5) a) 3.75 in IEE 754: 1.875"<<endl;
+    cout<<"5) b) 0.7 in IEE 754: 1.4"<<endl;
+    cout<<"5) c) 89.9 in IEE 754: 1.4046875"<<endl;
     
     //Exit the Program - Cleanup
     return 0;
